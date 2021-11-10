9eca2aa9ba2ad17e5161676bf155bc2e6c340ced
Evhenia Vepretskaya <enychka12@gmail.com>
2021-11-10 13:05:36 +0200

    3 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+const list = document.querySelector('ul');
+
+const elements = ingredients.map((elements) => {
+    const newEl = document.createElement('li');
+    newEl.textContent = elements;
+    return newEl;
+
+});
+
+list.append(...elements);
+console.log(list);
