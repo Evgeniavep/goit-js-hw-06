178f025a6e7a0518f77d0b548fcf3f74c505b54a
Evhenia Vepretskaya <enychka12@gmail.com>
2021-11-12 13:54:52 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const colorButton = document.querySelector('button')
+const bckgrColor = document.querySelector('span');
+const bodyStyle = document.querySelector("body")
+
+
+
+colorButton.addEventListener("click", changeColor);
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeColor() {
+    const bodyColor = getRandomHexColor();
+    bodyStyle.style.backgroundColor = bodyColor;
+    bckgrColor.textContent = bodyColor;
+
+};
