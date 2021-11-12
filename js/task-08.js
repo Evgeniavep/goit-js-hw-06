76d3b5831276cec7ae3703e0857d49393de021d4
Evhenia Vepretskaya <enychka12@gmail.com>
2021-11-12 13:34:12 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Attention! All the fields should be filled.");
+    }
+
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+};
